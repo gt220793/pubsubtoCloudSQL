0a13a89f4f8b4e535b226683512a164be9843d5f
Gourav.tripathi <gourav.tripathi@mobiliya.com>
2019-11-14 10:46:21 +0530

    Code to push json in Postgres-table with single column.

--- src/main/java/com/journaldev/bean/PipeLineTest.java
+++ src/main/java/com/journaldev/bean/PipeLineTest.java
@@ -1,51 +1,21 @@
 package com.journaldev.bean;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.net.URL;
-import java.sql.PreparedStatement;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
-
-import autovalue.shaded.com.google$.common.collect.$ForwardingList;
-import joptsimple.internal.Strings;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.io.jdbc.JdbcIO;
-import org.apache.beam.sdk.io.kafka.KafkaIO;
-import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.Values;
-import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.commons.io.FileUtils;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.common.serialization.Deserializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.SAXException;
+
+import java.sql.PreparedStatement;
 
 public class PipeLineTest {
 
@@ -58,6 +28,10 @@ public class PipeLineTest {
      * The Constant INPUT_FILE_NAME.
      */
     private static final String INPUT_FILE_NAME = "gs://pubsub-json-demo/sample_json.txt";
+    private static final String PROJECT_ID = "data-flow-runner-demo";
+    private static final String GCP_API_KEY = "D:/IntellIJ workspace/pubsub-assignments/data-flow-runner-demo-580a315ec0b4.json";
+    private static final String BUCKET_NAME = "pubsub-json-demo";
+    private static final String FILE_NAME = "sample_json.txt";
 
     /**
      * The Constant OUTPUT_TOPIC_NAME.
@@ -71,11 +45,19 @@ public class PipeLineTest {
      * @param args the arguments
      */
 
-       public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+
+        GoogleCredentials googleCredentials = CredentialsManager.loadGoogleCredentials(GCP_API_KEY);
+        Storage storage = StorageOptions.newBuilder()
+                .setProjectId(PROJECT_ID)
+                .setCredentials(googleCredentials)
+                .build()
+                .getService();
+        Blob blob = storage.get(BUCKET_NAME, FILE_NAME);
+        String json = new String(blob.getContent());
+
         Pipeline pipeline = Pipeline.create(PipelineOptionsFactory.fromArgs(args).withValidation().create());
-        File file = new File(INPUT_FILE_NAME);
-        PCollection<String> input = pipeline.apply(TextIO.read().from(INPUT_FILE_NAME));
-		 //  PCollection<String> input = pipeline.apply(file.read().from(INPUT_FILE_NAME));
+        PCollection<String> input = pipeline.apply("Read JSON from text file", Create.of(new String(json)));
         PCollection<String> output = input.apply("Read the file", ParDo.of(new DoFn<String, String>() {
             private static final long serialVersionUID = 1L;
 
@@ -94,25 +76,25 @@ public class PipeLineTest {
 
         output.apply(PubsubIO.writeStrings().to(OUTPUT_TOPIC_NAME));
 
-		PCollection<String> output_json = pipeline.apply(PubsubIO.readStrings().fromTopic(OUTPUT_TOPIC_NAME));
-		output_json.apply(JdbcIO.<String>write()
+        PCollection<String> output_json = pipeline.apply(PubsubIO.readStrings().fromTopic(OUTPUT_TOPIC_NAME));
+        output_json.apply(JdbcIO.<String>write()
                 .withDataSourceConfiguration(JdbcIO.DataSourceConfiguration
-						.create("org.postgresql.Driver", "jdbc:postgresql://localhost:5432/postgres")
-						.withUsername("postgres").withPassword("postgres"))
-				.withStatement("INSERT INTO JSON (complete_value)VALUES (?)")
+                        .create("org.postgresql.Driver", "jdbc:postgresql://localhost:5432/postgres")
+                        .withUsername("postgres").withPassword("postgres"))
+                .withStatement("INSERT INTO TEST (TEXTVALUE) VALUES (?)")
                 .withPreparedStatementSetter(new JdbcIO.PreparedStatementSetter<String>() {
 
-                    private static final long serialVersionUID = 1L;
+                                                 private static final long serialVersionUID = 1L;
 
-                    public void setParameters(String element, PreparedStatement preparedStatement) throws Exception {
-                        logger.info("DB Opertaion xml data: " + element);
-                        System.out.println(element);
-                        preparedStatement.setString(1, element);
-                    }
-                }
-				));
-		pipeline.run();
-		LOGGER.debug("All done!!");
+                                                 public void setParameters(String element, PreparedStatement preparedStatement) throws Exception {
+                                                     logger.info("DB Opertaion json data: " + element);
+                                                     System.out.println(element);
+                                                     preparedStatement.setString(1, element);
+                                                 }
+                                             }
+                ));
+        pipeline.run();
+        LOGGER.debug("All done!!");
     }
 }
 
